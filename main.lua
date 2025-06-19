960382af6acbed55f7c648c0250333df299eea4f
mohameddtrsuc9x <mohameddtrsuc9x@gmail.com>
2025-06-19 18:42:58 +0300

    Add files via upload

--- main.lua
+++ main.lua
@@ -28,9 +28,8 @@ Tab:AddToggle({
             wait(0.5)
             for i, value in pairs(game:GetService("Players").LocalPlayer.Backpack:GetChildren()) do
                 if value:IsA("Tool") then
-					print(string.gsub(tostring(value), " ",""))
                     game:GetService("ReplicatedStorage"):WaitForChild("Replicator"):InvokeServer(unpack({tostring(
-                        current_slot_name), string.gsub(tostring(value), " ",""), {
+                        current_slot_name), tostring(value):gsub(" ",""), {
                         MouseRay = {
                             Normal = Vector3.yAxis,
                             Direction = vector.create(-233.08799743652344, -114.48482513427734, 12.775901794433594),
